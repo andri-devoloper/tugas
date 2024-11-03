f6f4171f3bc2aa4f004ad6a88d1125ae8da564a5
andri-devoloper <a8461565@gmail.com>
2024-11-03 13:32:02 +0700

    Deskripsi perubahan nilai

--- app/guru/page.tsx
+++ app/guru/page.tsx
@@ -1,29 +1,15 @@
-'use client';
-import { useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+
+
 import Gurus from '@/components/Guru';
-import { Suspense } from 'react';
+
 
 export default function GuruPage() {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-
-    useEffect(() => {
-        // Ambil kunci akses dari search params
-        const accessKey = searchParams.get('key');
-
-        // Periksa apakah kunci akses sesuai
-        if (accessKey !== 'mySecretKey123') {
-            // Arahkan pengguna ke halaman error atau halaman lain
-            router.push('/error');
-        }
-    }, [router, searchParams]);
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <div>
-                <Gurus />
-            </div>
-        </Suspense>
+
+        <div>
+            <Gurus />
+        </div>
+
     );
 }

3e655af603eec0dcdccb7666f6aee5178187c145
andri-devoloper <a8461565@gmail.com>
2024-11-05 13:20:43 +0700

    Deskripsi perubahan nilai baru

--- app/guruTubamas09172812/page.tsx
+++ app/guruTubamas09172812/page.tsx
@@ -6,11 +6,10 @@ import Gurus from '@/components/Guru';
 export default function GuruPage() {
 
     return (
-        <div className='flex flex-row'>
-            <div>
-                <Gurus />
-            </div>
-          
+
+        <div>
+            <Gurus />
         </div>
+
     );
 }
